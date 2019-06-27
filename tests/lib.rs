6971772627623f0c32f97824f4815a9c14a4720a
Nathan Henrie <nate@n8henrie.com>
2019-06-27 16:12:18 -0600

    Ensure that `below` is not included

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,11 +3,11 @@ use primetools::*;
 #[test]
 fn test_primegen() {
     assert_eq!(
-        PrimeGen::new(Some(20)).collect::<Vec<_>>(),
+        PrimeGen::new(Some(23)).collect::<Vec<_>>(),
         [2, 3, 5, 7, 11, 13, 17, 19],
     );
 }
 #[test]
 fn test_primesieve() {
-    assert_eq!(PrimeSieve::new(20).0, [2, 3, 5, 7, 11, 13, 17, 19],);
+    assert_eq!(PrimeSieve::new(23).0, [2, 3, 5, 7, 11, 13, 17, 19],);
 }
